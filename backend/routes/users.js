2f4f05dd12f95f7c32376b9efb25d0c81d467890
Ayush <66142838+ayushtejas@users.noreply.github.com>
2024-07-29 04:48:59 +0000

    .

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -106,8 +106,8 @@ router.post('/register',(req,res)=>{
 
 router.get('/me', authenticateToken, (req,res)=>{
   user.findOne({_id: req.user.item.id}).then(
-    (item) => res.status(200).json(item)
-  ).catch(err=>res.status(404).json('user not found'))
+    (item) => {res.status(200).json(item)}
+  ).catch((err)=>{res.status(404).json('user not found')})
 })
 
 module.exports = router;
