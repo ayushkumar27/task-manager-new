c5b36a8c6522583456e70560d741792688adb576
Ayush Kumar <ayushkumar@Ayushs-Laptop.local>
2024-07-29 05:39:06 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,14 @@ mongoose.connect('mongodb+srv://ayushdincere99:Ayush123@cluster0.12lncob.mongodb
 app.use(cors())
 app.use(express.json())
 
+let corsOptions = {
+    origin: [ 'https://task-manager-new-orpin.vercel.app/', 'http://localhost:3000' ]
+};
+
+// Using cors as a middleware
+app.get('/gfg-articles',cors(corsOptions),
+    (req,res) => res.json('gfg-articles'))
+
 const user = require('./models/userModel')
 const loginRoute = require('./routes/users')
 const tasksRoute = require('./routes/tasks')
